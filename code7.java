d8dc8e3ec1abaf3ff7413fdc38ee4e125bd46481
Kiran H K <kiranhkkiranhk60@gmail.com>
2026-01-29 15:45:47 +0530

    Add files via upload

--- code7.java
+++ code7.java
@@ -1,5 +1,13 @@
-package day2;
-
 public class code7 {
-
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        if (a>b){
+            System.out.println(a);
+        
+        }
+        else{
+            System.out.println(b);
+        }
+    }
 }
---
+++ code8.java
@@ -0,0 +1,9 @@
+public class code8 {
+    public static void main(String[] args) {
+        int a=10;
+        int b=20;
+        int c=2;
+        double si=(a*b*c)/100;
+        System.out.println(si);
+    }
+}
